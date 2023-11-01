3177121c2f8909d6b84583bc8edf7cd802728659
Adrián Litardo <lalitard@espol.edu.ec>
2023-11-01 10:53:19 -0500

    añadiendo graficos

--- dashboard_template/public/javascript/load_data.js
+++ dashboard_template/public/javascript/load_data.js
@@ -40,3 +40,99 @@ let cargarFechaActual = () => {
     tituloH6.textContent = fechaActual()
 }
 cargarFechaActual()
+
+let cargarOpenMeteo = () => {
+
+    //URL que responde con la respuesta a cargar
+    let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=temperature_2m&timezone=auto';
+
+    fetch(URL)
+        .then(responseText => responseText.json())
+        .then(responseJSON => {
+
+            console.log(responseJSON);
+
+            //Respuesta en formato JSON.
+
+            //Respuesta en formato JSON
+            //Referencia al elemento con el identificador plot
+            let plotRef = document.getElementById('plot1');
+
+            //Etiquetas del gráfico
+            let labels = responseJSON.hourly.time;
+
+            //Etiquetas de los datos
+            let data = responseJSON.hourly.temperature_2m;
+
+            //Objeto de configuración del gráfico
+            let config = {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: 'Temperature [2m]',
+                            data: data,
+                        }
+                    ]
+                }
+            };
+
+            //Objeto con la instanciación del gráfico
+            let chart1 = new Chart(plotRef, config);
+
+
+        })
+        .catch(console.error);
+
+}
+let cargarOpenMeteo2 = () => {
+
+    //URL que responde con la respuesta a cargar
+    let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=precipitation_probability&timezone=auto';
+
+    fetch(URL)
+        .then(responseText => responseText.json())
+        .then(responseJSON => {
+
+            console.log(responseJSON);
+
+            //Respuesta en formato JSON.
+
+            //Respuesta en formato JSON
+            //Referencia al elemento con el identificador plot
+            let plotRef = document.getElementById('plot2');
+
+            //Etiquetas del gráfico
+            let labels = responseJSON.hourly.time;
+
+            //Etiquetas de los datos
+            let data = responseJSON.hourly.precipitation_probability;
+
+            //Objeto de configuración del gráfico
+            let config = {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: 'Temperature [2m]',
+                            data: data,
+                        }
+                    ]
+                }
+            };
+
+            //Objeto con la instanciación del gráfico
+            let chart1 = new Chart(plotRef, config);
+
+
+        })
+        .catch(console.error);
+
+}
+
+cargarPrecipitacion()
+cargarFechaActual()
+cargarOpenMeteo()
+cargarOpenMeteo2()
